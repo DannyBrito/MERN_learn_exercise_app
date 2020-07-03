c4786c09df376b6690d50386f8cb96b7c6933b38
DannyBrito <britodanny1997@gmail.com>
2020-07-03 18:38:28 -0400

    connection.js handles mongoose connection to db instead of app.js

---
+++ backend/connetion.js
@@ -0,0 +1,13 @@
+const app = require('./app');
+const mongoose = require('mongoose')
+
+require('dotenv').config();
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useUnifiedTopology:true,useNewUrlParser: true, useCreateIndex: true});
+
+const connection = mongoose.connection;
+connection.once('open',()=>{
+    console.log("MongoDB database connection establish succesfully")
+});
+
