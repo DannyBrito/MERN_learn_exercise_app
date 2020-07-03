40e2c4908facdf142d338115212931cacf79f526
DannyBrito <britodanny1997@gmail.com>
2020-07-03 19:45:00 -0400

    User routing tests added w/ jest

---
+++ backend/tests/helper/server-handler.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+module.exports ={    
+    mongoose,
+    connect:()=>{
+        require('../../connetion')
+    },
+    disconnect: done =>{
+        mongoose.disconnect(done)
+    }
+}
